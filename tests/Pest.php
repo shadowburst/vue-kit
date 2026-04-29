34ecb8784969ebfa96f6aa17eb8173103ac1c419
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2026-04-29 23:24:06 +0200

    ci: add main workflow

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 /*
@@ -17,7 +18,7 @@ use Tests\TestCase;
 
 pest()
     ->extend(TestCase::class)
-    // ->use(RefreshDatabase::class)
+    ->use(RefreshDatabase::class)
     ->in('Feature');
 
 /*
