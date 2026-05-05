f252d66bde8c5db876c22dcbe57759145760f926
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2026-05-05 12:02:35 +0200

    fix(i18n): add settings to users table

--- database/migrations/2026_05_05_044749_add_settings_to_users_table.php
+++ database/migrations/2026_05_05_044749_add_settings_to_users_table.php
@@ -1,15 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->json('settings')->nullable()->after('remember_token');
+            $table->jsonb('settings')->nullable()->after('remember_token');
         });
     }
 
